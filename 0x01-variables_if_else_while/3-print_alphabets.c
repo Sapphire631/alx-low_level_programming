612246b27f89db4d50056baa84be818aa6da4e4c
sapphire631 <blessing.nwafor631@gmail.com>
2022-09-10 19:56:50 -0700

    3-alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
 
 /**
-
  * main - entry point
-
  *
-
  * Return: alway returns 0
-
- *
-
  */
 
 int main(void)
-  
 {
-  
-  char a;
-  
-
+  char a; 
   
   for (a = 'a'; a <= 'z' ; a++)
     
