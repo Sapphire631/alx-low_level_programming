1444e7a7279f06cf4bd94ff0c3fe1c74373e7408
Sapphire631 <blessing.nwafor631@gmail.com>
2022-09-19 22:52:51 -0700

    fixed errors on 6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,20 @@
 #include "main.h"
-#include "2-strlen.c"
 
 /**
- * puts2 - puts string twice
- * @str: string to put twice
- * Return: void 
+ * puts2 - prints part of a string
+ * @str: string to be printed
  *
- */
+ * Return: void
+ **/
 void puts2(char *str)
 {
-        int i;
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
 
-        for (i = 0; i < _strlen(str); i++)
-        {
-                if (i % 2 == 0)
-                {
-                        _putchar(str[i]);
-                }
-        }
-        _putchar('\n');
+_putchar('\n');
 }
