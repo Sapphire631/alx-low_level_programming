8dcfe7e7410add5ecbc888486aac172f6082b964
Sapphire631 <blessing.nwafor631@gmail.com>
2022-09-26 20:15:33 -0700

    fixed errors on diagmus

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,10 +1,11 @@
+
 #include <stdio.h>
 #include "main.h" 
 
-/** * 
+/**
 *print_diagsums - prints the sum of the two diagonals 
 * of a square matrix of integers
- * @a: square matrix of which we print the sum of diagonals 
+* @a: square matrix of which we print the sum of diagonals 
 * @size: size of the matrix 
 */void print_diagsums(int *a, int size)
 {	
