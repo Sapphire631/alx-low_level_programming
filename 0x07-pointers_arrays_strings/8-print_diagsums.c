f6e9651af81747eb9024a8adfb5399bf2fdedec9
Sapphire631 <blessing.nwafor631@gmail.com>
2022-09-26 20:36:23 -0700

    diagmus fixed errors

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include "main.h" 
-
 /**
 *print_diagsums - prints the sum of the two diagonals 
 * of a square matrix of integers
 * @a: square matrix of which we print the sum of diagonals 
-*/ @size: size of the matrix
- 
-void print_diagsums(int *a, int size)
+* @size: size of the matrix
+*/ void print_diagsums(int *a, int size)
+void print_diagsums(int *a, int size);
 {	
 int i;	
 unsigned int sum, sum1; 
@@ -20,4 +19,3 @@ for (i = 0; i < size; i++)
 	}
 	printf("%d, %d\n", sum, sum1);
 }
-
