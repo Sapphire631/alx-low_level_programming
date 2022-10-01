eca1d6ef2f59b57fc1c1adddc5f26d36a8b70b19
Sapphire631 <blessing.nwafor631@gmail.com>
2022-10-01 02:21:57 -0700

    the last two tasks

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,51 +1,34 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 /**
- * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- *Return: always 0
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ *         Otherwise - 0.
  */
-int main(int argc, char  *argv[])
+int main(int argc, char *argv[])
 {
-	int coins = 0;
+	int num, digit, sum = 0;
 
-	if (argc == 2)
+	for (num = 1; num < argc; num++)
 	{
-		if (strchr(argv[argc - 1], '-'))
+		for (digit = 0; argv[num][digit]; digit++)
 		{
-			printf("0\n");
-			return (1);
-		}
-		int money;
-
-		money = atoi(argv[argc - 1]);
-
-		while (money > 0)
-		{
-			if (money % 25 == 0)
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
 			{
-				money -= 25;
-			} else if (money % 10 == 0)
-			{
-				money -= 10;
-			} else if (money % 5 == 0)
-			{
-				money -= 5;
-			} else if (money % 2 == 0)
-			{
-				money -= 2;
-			} else
-			{
-				money--;
+				printf("Error\n");
+				return (1);
 			}
-			coins++;
 		}
-		printf("%d\n", coins);
-		return (0);
+
+		sum += atoi(argv[num]);
 	}
-	printf("Error\n");
-	return (1);
+
+	printf("%d\n", sum);
+
+	return (0);
 }
